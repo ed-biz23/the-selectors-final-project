93bfb70ba082aed10ad94276f975bae55b1ff25b
Edward Biswas <edbiswas@gmail.com>
2019-05-23 19:43:55 -0400

    Updates

---
+++ precache-manifest.2b1863d853435079487793201782e5f4.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "92fbf20df54c47e9e44c5656af2ad93e",
+    "url": "/the-selectors-final-project/index.html"
+  },
+  {
+    "revision": "4e71b7e173222a838b0b",
+    "url": "/the-selectors-final-project/static/css/2.266e55a5.chunk.css"
+  },
+  {
+    "revision": "4e71b7e173222a838b0b",
+    "url": "/the-selectors-final-project/static/js/2.4648cb7f.chunk.js"
+  },
+  {
+    "revision": "fab93e1ea2584cbae193",
+    "url": "/the-selectors-final-project/static/js/main.846f1f3e.chunk.js"
+  },
+  {
+    "revision": "f31f5ef6e6024b2721d5",
+    "url": "/the-selectors-final-project/static/js/runtime~main.f31bf015.js"
+  },
+  {
+    "revision": "78c4b56e2a9033cced2fb65bcb52a7df",
+    "url": "/the-selectors-final-project/static/media/nyc.78c4b56e.jpg"
+  }
+]);
